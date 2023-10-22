3e015ceb8cc62a35826a9eebaac186de845787df
louispo0603 <118866618+louispo0603@users.noreply.github.com>
2023-10-22 14:08:09 -0400

    sqlite instead of postgre

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,17 +78,24 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if "RDS_DB_NAME" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.environ["RDS_DB_NAME"],
-            "USER": os.environ["RDS_USERNAME"],
-            "PASSWORD": os.environ["RDS_PASSWORD"],
-            "HOST": os.environ["RDS_HOSTNAME"],
-            "PORT": os.environ["RDS_PORT"],
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
+# if "RDS_DB_NAME" in os.environ:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql_psycopg2",
+#             "NAME": os.environ["RDS_DB_NAME"],
+#             "USER": os.environ["RDS_USERNAME"],
+#             "PASSWORD": os.environ["RDS_PASSWORD"],
+#             "HOST": os.environ["RDS_HOSTNAME"],
+#             "PORT": os.environ["RDS_PORT"],
+#         }
+#     }
 # else:
 #     DATABASES = {
 #         "default": {
