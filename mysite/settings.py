fc90a6016ead15167dbb3575898c2b26f00bdab7
louispo0603 <118866618+louispo0603@users.noreply.github.com>
2023-10-22 13:40:32 -0400

    try postgre on travis

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,10 +92,15 @@ if "RDS_DB_NAME" in os.environ:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "django-polls",
+            "USER": "postgres",
+            "PASSWORD": "password1126",
+            "HOST": "localhost",
+            "PORT": "5432",
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
